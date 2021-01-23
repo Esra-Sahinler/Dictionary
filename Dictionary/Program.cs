f2b4b19d8de6ea25feafd0b111d37e42f41509ed
Esra-Sahinler <esrasahinler4@gmail.com>
2021-01-23 19:47:11 +0300

    Proje dosyası ekle.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> dictonary = new MyDictionary<string, int>();
+
+            dictonary.Add("Esra", 1);
+            dictonary.Add("Ceren", 2);
+            dictonary.Add("Mehtap", 3);
+            Console.WriteLine(dictonary.Count);
+            Console.ReadKey();
+        }
+    }
+}
